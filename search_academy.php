4ff879c56e83a80621fe5ddb56e7060f890a79a4
lucas-satoshi <lucas.satoshi.18@gmail.com>
2025-01-04 23:37:46 -0300

    search academy

---
+++ search_academy.php
@@ -0,0 +1,31 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+// Conexão com o banco de dados
+$conn = new mysqli("193.203.175.154:3306", "u196497167_admin", "OculosEscuro@132", "u196497167_fenixreborn");
+
+
+if ($conn->connect_error) {
+    echo json_encode(["erro" => "Erro de conexão"]);
+    exit;
+}
+
+// Obtenha o termo de pesquisa
+$termo = "%" . $_GET['termo'] . "%"; // Use '%' para busca parcial
+
+$sql = "SELECT * FROM academy WHERE nome LIKE ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $termo);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$cursos = [];
+while ($row = $result->fetch_assoc()) {
+    $cursos[] = $row;
+}
+
+echo json_encode($cursos);
+$stmt->close();
+$conn->close();
+?>
