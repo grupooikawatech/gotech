9292d76e19604677e0ca15dcb5d85cdaf2bef37e
caioasmuniz <caiomuniz888@gmail.com>
2025-04-04 16:57:21 -0300

    adds tailwind classes to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,19 @@ import Portifolio from "./components/portifolio"
 import AboutUs from "./components/aboutUs"
 import FormsAgendamento from "./components/formsAgendamento"
 
-export default function Page() {
+export default function Home() {
   return (
-    <div>
-      <h1>Fenix Reborn</h1>
-      <h2>
+    <div className="mx-auto max-w-2xl">
+      <h2 className="max-w-2xl text-center">
         Navegue pelo nosso portfólio e veja como a Fênix Reborn dá vida às ideias, criando soluções digitais que impressionam e conquistam.
       </h2>
       <Portifolio />
-      <h2>
+      <h2 className="max-w-2xl text-center">
         Conheça a Fênix Reborn e o time que torna a transformação digital uma realidade para o seu negócio.
       </h2>
       <AboutUs />
-      <h2>
-      Dê vida ao seu negócio com nossa ajuda especializada.
+      <h2 className="max-w-2xl text-center">
+        Dê vida ao seu negócio com nossa ajuda especializada.
       </h2>
       <FormsAgendamento />
     </div>
