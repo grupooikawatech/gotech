f0296ca3965a7a51a00f5007dff9baaeccddefbb
caioasmuniz <caiomuniz888@gmail.com>
2025-04-04 16:41:15 -0300

    adds custom Inter font site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({ subsets: ['latin'] })
+
 export default function HomepageLayout(
   { children, }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.className}`}>
         <main>{children}</main>
       </body>
     </html>
