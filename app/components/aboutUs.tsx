17a0af33385c402f61f5f3839ea32ffbc3c4b7c7
caioasmuniz <caiomuniz888@gmail.com>
2025-04-04 16:54:45 -0300

    initial tailwind impl on aboutUs and portifolio components

--- app/components/aboutUs.tsx
+++ app/components/aboutUs.tsx
@@ -1,18 +1,20 @@
-import Image from 'next/image'
 function generateStaticParams() { }
 
-export default function Page() {
+export default function AboutUs() {
     return (
-        <section>
-            <h2>Sobre Nós</h2>
+        <section
+            className="mx-auto max-w-2xl items-center gap-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
+        >
+            <h2 className='text-xl font-medium text-black dark:text-white'
+            >Sobre Nós</h2>
             <p>
-            A Fênix Reborn nasceu em 2024, fruto da união de um time de profissionais apaixonados por marketing, web design e programação. Com anos de estudo e prática, decidimos transformar todo esse conhecimento em algo que fizesse a diferença no mercado digital. Nosso objetivo é simples: criar soluções inovadoras que não apenas atendem às necessidades dos nossos clientes, mas que também impactam positivamente os negócios, contribuindo para um mercado digital mais dinâmico e transformador.
+                A Fênix Reborn nasceu em 2024, fruto da união de um time de profissionais apaixonados por marketing, web design e programação. Com anos de estudo e prática, decidimos transformar todo esse conhecimento em algo que fizesse a diferença no mercado digital. Nosso objetivo é simples: criar soluções inovadoras que não apenas atendem às necessidades dos nossos clientes, mas que também impactam positivamente os negócios, contribuindo para um mercado digital mais dinâmico e transformador.
             </p>
             <p>
-            Nosso compromisso é com a transformação de ideias em negócios de sucesso, utilizando tecnologia de ponta, design criativo e estratégias de marketing eficazes para proporcionar resultados concretos. Acreditamos que cada projeto é uma oportunidade única de fazer a diferença, e trabalhamos com paixão para entregar soluções que realmente conectem as marcas aos seus públicos e tragam crescimento real.
+                Nosso compromisso é com a transformação de ideias em negócios de sucesso, utilizando tecnologia de ponta, design criativo e estratégias de marketing eficazes para proporcionar resultados concretos. Acreditamos que cada projeto é uma oportunidade única de fazer a diferença, e trabalhamos com paixão para entregar soluções que realmente conectem as marcas aos seus públicos e tragam crescimento real.
             </p>
             <p>
-            Na Fênix Reborn, acreditamos no poder da colaboração e da inovação para levar as empresas ao próximo nível. Estamos aqui para ajudar você a alcançar seus objetivos digitais com qualidade, criatividade e dedicação.
+                Na Fênix Reborn, acreditamos no poder da colaboração e da inovação para levar as empresas ao próximo nível. Estamos aqui para ajudar você a alcançar seus objetivos digitais com qualidade, criatividade e dedicação.
             </p>
         </section>
     )
--- app/components/portifolio.tsx
+++ app/components/portifolio.tsx
@@ -1,37 +1,49 @@
 import Image from 'next/image'
-function generateStaticParams() { }
 
-export default function Page() {
-    return (
-        <section>
-            <h2> Portifólio </h2>
-            <div>
-                <h3>Venda Certa</h3>
-                <Image
-                    src="/venda-certa.png"
-                    width={100}
-                    height={100}
-                    alt=''
-                />
-            </div>
-            <div>
-                <h3>Clínica Oikawa</h3>
-                <Image
-                    src="/clinica_oikawa.webp"
-                    width={100}
-                    height={100}
-                    alt=''
-                />
-            </div>
-            <div>
-                <h3>Patch News</h3>
-                <Image
-                    src="/logo-patch-news.png"
-                    width={100}
-                    height={100}
-                    alt=''
-                />
-            </div>
-        </section>
-    )
+export default function Portifolio() {
+  return (
+    <section
+      className="mx-auto max-w-2xl rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
+    >
+      <h2
+        className="text-xl font-medium text-black dark:text-white"
+      > Portifólio </h2>
+      <div className='flex justify-center gap-4 items-center'>
+        <button
+          className='rounded-xl bg-white hover:bg-blue-300 p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 hover:bg-blue'
+        >
+          <h3>Venda Certa</h3>
+          <Image
+            className='aspect-square object-cover'
+            src="/venda-certa.png"
+            width={150}
+            height={150}
+            alt=''
+          />
+        </button>
+        <button className='rounded-xl bg-white hover:bg-blue-300 p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'
+        >
+          <h3>Clínica Oikawa</h3>
+          <Image
+            className='aspect-square object-cover'
+            src="/clinica_oikawa.webp"
+            width={150}
+            height={150}
+            alt=''
+          />
+        </button>
+        <button className='rounded-xl bg-white hover:bg-blue-300 p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'
+        >
+          <h3>Patch News</h3>
+          <Image
+            className='aspect-square object-cover'
+            src="/logo-patch-news.png"
+            width={150}
+            height={150}
+            alt=''
+          />
+        </button>
+      </div>
+    </section>
+  )
 }
