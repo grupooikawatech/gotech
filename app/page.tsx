8b3ed9eb4eb9776bf77151ecd867cef7d22dd0da
caioasmuniz <caiomuniz888@gmail.com>
2025-04-09 13:15:28 -0300

    refactored home page location for more cleaniness

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,2 @@
-import Portifolio from "./ui/portifolio"
-import AboutUs from "./ui/aboutUs"
-import FormsAgendamento from "./ui/formsAgendamento"
-
-export default function Home() {
-  return (
-    <div className="mx-auto max-w-2xl">
-      <h2 className="max-w-2xl text-center">
-        Navegue pelo nosso portfólio e veja como a Fênix Reborn dá vida às ideias, criando soluções digitais que impressionam e conquistam.
-      </h2>
-      <Portifolio />
-      <h2 className="max-w-2xl text-center">
-        Conheça a Fênix Reborn e o time que torna a transformação digital uma realidade para o seu negócio.
-      </h2>
-      <AboutUs />
-      <h2 className="max-w-2xl text-center">
-        Dê vida ao seu negócio com nossa ajuda especializada.
-      </h2>
-      <FormsAgendamento />
-    </div>
-  )
-}
+import Page from "./_home/page";
+export default Page
