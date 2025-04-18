452fe398448474add65a4b4b5df512c2724578bf
caioasmuniz <caiomuniz888@gmail.com>
2025-04-18 19:37:35 -0300

    style revamp in portifolio preparing for modals

--- app/_home/components/portifolio.tsx
+++ app/_home/components/portifolio.tsx
@@ -1,18 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Portifolio() {
   return (
-    <section
+    <div
       className="mx-auto max-w-2xl rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
     >
-      <h2
-        className="text-xl font-medium text-black dark:text-white"
-      > Portifólio </h2>
+      <h2 className="text-3xl text-center mb-4 font-medium text-black dark:text-white"
+      >{"Portifólio"}</h2>
       <div className='flex justify-center gap-4 items-center'>
-        <button
+        <Link
+          href={"/portifolio/venda-certa"}
           className='rounded-xl bg-white hover:bg-blue-300 p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 hover:bg-blue'
         >
-          <h3>Venda Certa</h3>
+          <h3 className="text-xl text-center mb-2">{"Venda Certa"}</h3>
           <Image
             className='aspect-square object-cover'
             src="/venda-certa.png"
@@ -20,10 +21,13 @@ export default function Portifolio() {
             height={150}
             alt=''
           />
-        </button>
-        <button className='rounded-xl bg-white hover:bg-blue-300 p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'
+        </Link>
+
+        <Link
+          href={"/portifolio/clinica-oikawa"}
+          className='rounded-xl bg-white hover:bg-blue-300 p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 hover:bg-blue'
         >
-          <h3>Clínica Oikawa</h3>
+          <h3 className="text-xl text-center mb-2">{"Clínica Oikawa"}</h3>
           <Image
             className='aspect-square object-cover'
             src="/clinica_oikawa.webp"
@@ -31,10 +35,13 @@ export default function Portifolio() {
             height={150}
             alt=''
           />
-        </button>
-        <button className='rounded-xl bg-white hover:bg-blue-300 p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'
+        </Link>
+
+        <Link
+          href={"/portifolio/patch-news"}
+          className='rounded-xl bg-white hover:bg-blue-300 p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 hover:bg-blue'
         >
-          <h3>Patch News</h3>
+          <h3 className="text-xl text-center mb-2">{"Patch News"}</h3>
           <Image
             className='aspect-square object-cover'
             src="/logo-patch-news.png"
@@ -42,8 +49,8 @@ export default function Portifolio() {
             height={150}
             alt=''
           />
-        </button>
+        </Link>
       </div>
-    </section>
+    </div >
   )
 }
