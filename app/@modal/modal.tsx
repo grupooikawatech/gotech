88f1aa666db91b1c7f6af1bbcad8f69c7e28a565
caioasmuniz <caiomuniz888@gmail.com>
2025-04-18 19:32:01 -0300

    style fixes in modal

--- app/@modal/modal.tsx
+++ app/@modal/modal.tsx
@@ -16,23 +16,23 @@ export default function Modal({ children }: {
   }, []);
 
   return createPortal(
-    // <div className="modal-backdrop">
     <dialog
       ref={dialogRef}
-      className="m-auto max-w-2xl rounded-xl bg-white p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
+      className="flex flex-col items-end m-auto
+       max-w-2xl rounded-xl bg-white p-4 shadow-lg 
+       outline outline-black/5 dark:bg-slate-800 
+       dark:shadow-none dark:-outline-offset-1
+        dark:outline-white/10 text-black dark:text-white"
       onClose={() => router.back()}
     >
-      <div className="flex">
-        <button
-          onClick={() => router.back()}
-          className="rounded-full m-auto p-2 shadow-lg bg-red-400"
-        >
-          <XMarkIcon className="w-4" />
-        </button>
-      </div>
+      <button
+        onClick={() => router.back()}
+        className="rounded-full p-2 cursor-pointer shadow-lg bg-red-400"
+      >
+        <XMarkIcon className="w-4" />
+      </button>
       {children}
     </dialog>
-    // </div>
     , document.getElementById('modal-root')!
   );
 }
