44af60e85520227a3e27c5fc6b91984a8d31b670
caioasmuniz <caiomuniz888@gmail.com>
2025-05-07 17:20:20 -0300

    init prisma for ORM

--- flake.nix
+++ flake.nix
@@ -9,8 +9,22 @@
       pkgs = import nixpkgs { inherit system; };
     in {
       devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ nodejs docker docker-compose ];
-        shellHook = "";
+        buildInputs = with pkgs; [
+          nixd
+          nixfmt
+          nodejs
+          docker
+          docker-compose
+          prisma-engines
+          openssl
+        ];
+        shellHook = ''
+          export PKG_CONFIG_PATH="${pkgs.openssl}/lib/pkgconfig";
+          export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+        '';
       };
     };
 }
